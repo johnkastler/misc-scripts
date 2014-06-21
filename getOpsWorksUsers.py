b340332568720de2c6be3c6ae790e744bba0179e
John Kastler <johnkastler@users.noreply.github.com>
2014-06-21 04:06:24 -0400

    Create getOpsWorksUsers.py

---
+++ getOpsWorksUsers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from boto import connect_opsworks
+from keyring import get_password
+
+accounts = {'aws-dev': 'XXXXXXXXXXXXXXXXXXXX',
+            'aws-qa': 'XXXXXXXXXXXXXXXXXXXX',
+            'aws-staging': 'XXXXXXXXXXXXXXXXXXXX',
+            'aws-demo': 'XXXXXXXXXXXXXXXXXXXX',
+            'aws-prod': 'XXXXXXXXXXXXXXXXXXXX'}
+
+def OpsWorksConn(account, id):
+    key = get_password(account, id)
+    conn = connect_opsworks(id, key)
+    return(conn)
+
+for account in accounts:
+    id = accounts[account]
+    conn = OpsWorksConn(account, id)
+    profiles = conn.describe_user_profiles().get('UserProfiles')
+    for profile in profiles:
+        name = profile.get('SshUsername')    
+        selfman = profile.get('AllowSelfManagement')
+        sshkey = profile.get('SshPublicKey')
+        
+        print("{0}, {3}, Self-Manage: {1}, SSH Key: {2}".format(name, selfman, sshkey, account))
